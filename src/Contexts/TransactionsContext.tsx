22dc752fbf4a73012acb6a13c5054e17bc8c2203
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2024-01-17 22:38:19 -0300

    ✨feat: useCallback hook added in createTansaction funcion

--- src/Contexts/TransactionsContext.tsx
+++ src/Contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 import { api } from '../lib/axios'
 import { createContext } from 'use-context-selector'
 
@@ -44,19 +44,21 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
 
     setTransactions(response.data)
   }
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date()
+      })
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date()
-    })
-
-    setTransactions(state => [response.data, ...state])
-  }
+      setTransactions(state => [response.data, ...state])
+    },
+    []
+  )
 
   useEffect(() => {
     fetchTransactions()
